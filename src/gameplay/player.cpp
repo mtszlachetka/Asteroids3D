10d96ab0eff9c1a2dfc6d9550069200fb92c8632
Mateusz Szlachetka <mtszlachetka@gmail.com>
2024-01-22 00:00:24 +0100

    fixed z axis rotation

--- src/gameplay/player.cpp
+++ src/gameplay/player.cpp
@@ -62,13 +62,13 @@ namespace se {
 				break;
 			}
 			case input_event::q_pressed: {
-				v3 direction = glm::toMat4(m_orientation) * glm::vec4(0,0,1,0);
-				m_orientation = m_orientation * glm::angleAxis(-m_anglespeed, direction);
+				v3 direction = v3(glm::toMat4(m_orientation) * glm::vec4(0,0,1,0));
+				m_orientation = glm::angleAxis(-m_anglespeed, direction) * m_orientation;
 				break;
 			}
 			case input_event::e_pressed: {
-				v3 direction = glm::toMat4(m_orientation) * glm::vec4(0,0,1,0);
-				m_orientation = m_orientation * glm::angleAxis(m_anglespeed, direction);
+				v3 direction = v3(glm::toMat4(m_orientation) * glm::vec4(0,0,1,0));
+				m_orientation = glm::angleAxis(m_anglespeed, direction) * m_orientation;
 				break;
 			}
 			case input_event::space_pressed:
