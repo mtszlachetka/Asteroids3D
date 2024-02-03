d08df25a689c220d52a385357dfb2094ef71e830
yanvoi <janwojciechowski@op.pl>
2024-02-03 12:33:54 +0100

    git added the missing files

---
+++ include/subengines/particle_engine.hpp
@@ -0,0 +1,72 @@
+#ifndef PARTICLE_ENGINE_HPP
+#define PARTICLE_ENGINE_HPP
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <list>
+#include <memory>
+#include "shader.hpp"
+#include "read_file.hpp"
+#include "texture.hpp"
+#include "subengines/gameplay_engine.hpp"
+
+namespace se {
+    struct particle_create_info {
+        glm::vec3 position, velocity, acceleration;
+        float lifetime;
+        glm::vec3 color;
+    };
+
+    class particle {
+        public:
+            glm::vec3 position, velocity, acceleration;
+            float t, lifetime;
+            glm::vec3 color;
+            glm::vec4 tint;
+            glm::mat4 model_transform;
+            particle(particle_create_info* create_info) {
+                this->position = create_info->position;
+                this->velocity = create_info->velocity;
+                this->acceleration = create_info->acceleration;
+                this->color = create_info->color;
+                this->lifetime = create_info->lifetime;
+                t = 0.0f;
+                model_transform = glm::mat4(1.0f);
+                model_transform = glm::translate(model_transform, position);
+            }
+            void update(float rate) {
+                velocity += rate * acceleration;
+                position += rate * velocity;
+
+                model_transform = glm::mat4(1.0f);
+                model_transform = glm::translate(model_transform, position);
+
+                t += rate;
+                tint = glm::vec4(color, 1.0f - (t / lifetime));
+            }
+    };
+
+    class particle_engine {
+        private:
+            se::mesh particle_mesh;
+            GLuint program;
+            GLuint vao, vbo;
+            std::list<std::unique_ptr<se::particle>> particle_ptrs;
+            void make_particles(glm::vec3 position, glm::vec3 incident, glm::vec3 normal);
+            void update();
+            particle_engine() {}
+        public:
+			particle_engine(const particle_engine& other) = delete;
+			particle_engine(const particle_engine&& other) = delete;
+			void operator=(const particle_engine& other) = delete;
+			void operator=(const particle_engine&& other) = delete;
+			static particle_engine& get_instance() {
+				static particle_engine instance;
+				return instance;
+			}
+			void init();
+            void render();
+    };
+}
+#endif
---
+++ src/subengines/particle_engine.cpp
@@ -0,0 +1,73 @@
+#include "subengines/particle_engine.hpp"
+#include "subengines/gameplay_engine.hpp"
+#include "mesh.hpp"
+#include "texture.hpp"
+#include "read_file.hpp"
+#include "shader.hpp"
+#include "SOIL.h"
+#include "clock.hpp"
+#include <memory>
+#include <random>
+#include <chrono>
+
+namespace se {
+    void particle_engine::init() {
+        particle_mesh = se::load_model("../models/sphere.obj");
+
+        // TODO: implement initialization for the instanced rendering of particles
+        // GLuint particle_vert = se::shader_from_string(GL_VERTEX_SHADER, se::read_file("../shaders/pass.vert"));
+        // GLuint particle_frag = se::shader_from_string(GL_FRAGMENT_SHADER, se::read_file("../shaders/pass.frag"));
+
+        // program = se::make_program({particle_vert, particle_frag});
+    }
+
+    void particle_engine::render() {
+        update();
+
+        // TODO: inmplement instanced rendering
+    }
+
+    void particle_engine::update() {
+        // glm::vec3 player_position = se::gameplay_engine::get_instance().get_player_position();
+        glm::vec3 player_position = glm::vec3(0, 0, 2);
+        make_particles(player_position, glm::normalize(glm::vec3(1, 0, 1)), glm::vec3(0, 0, -1)); // TODO: get other info on player
+
+        particle_ptrs.remove_if([](std::unique_ptr<particle>& p) -> bool { return p->t >= p->lifetime; });
+
+        float rate = 10; // TODO: make it dependent on player's speed
+        for (auto it = particle_ptrs.begin(); it != particle_ptrs.end(); ) {
+            se::particle* p = it->get();
+            p->update(rate);
+            ++it;
+        }
+    }
+
+    void particle_engine::make_particles(glm::vec3 position, glm::vec3 incident, glm::vec3 normal) {
+        unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+        std::default_random_engine generator(seed);
+
+        particle_create_info create_info;
+        create_info.acceleration = glm::vec3(0, 0, -0.001);
+        create_info.color = glm::vec3(1.0);
+        create_info.position = position;
+
+        for (int i = 0; i < 10; ++i) {
+            float x = float(generator() % 100) / 50.f - 1.0f;
+            float y = float(generator() % 100) / 50.f - 1.0f;
+            float z = float(generator() % 100) / 50.f - 1.0f;
+
+            glm::vec3 randomization = glm::vec3(x, y, z);
+            glm::vec3 randomized_normal = glm::normalize(0.9f * randomization + normal);
+
+            x = float(generator() % 100) / 10000.0f;
+            glm::vec3 outgoing = x * glm::reflect(incident, randomized_normal);
+
+            create_info.velocity = outgoing;
+
+            x = 50.0f + float(generator() % 100) / 5.0f;
+            create_info.lifetime = x;
+
+            particle_ptrs.push_back(std::make_unique<se::particle>(&create_info));
+        }
+    }
+}
