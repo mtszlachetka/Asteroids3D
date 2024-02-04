a6f8985a21177f9fc6ded20f91a118e8029dedc7
Mateusz Szlachetka <mtszlachetka@gmail.com>
2024-02-04 16:31:44 +0100

    particles leave from exhaust

--- src/subengines/particle_engine.cpp
+++ src/subengines/particle_engine.cpp
@@ -121,9 +121,12 @@ namespace se {
         glm::quat player_orientation = se::gameplay_engine::get_instance().get_player()->get_orientation();
         glm::mat3 rotation = glm::toMat3(player_orientation);
         glm::vec3 direction = rotation[2];
-        create_info.acceleration = glm::vec3(0, 0, -0.001);
+        create_info.acceleration = glm::vec3(0, 0, -0.001) * (-direction);
         create_info.color = glm::vec3(0.f, 1.f, 1.f);
+
+		position -= direction;
         create_info.position = position;
+
 
         int particles_to_generate = 1000;
         if (se::gameplay_engine::get_instance().get_player()->is_boosting()) {
